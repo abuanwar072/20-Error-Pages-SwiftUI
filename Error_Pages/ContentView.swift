51922e71ed3b17ed766d288cc7c7940051bde432
Abu Anwar <abuanwar072@gmail.com>
2021-01-10 23:33:10 +0600

    Initial Commit

---
+++ Error_Pages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Error_Pages
+//
+//  Created by Abu Anwar MD Abdullah on 10/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Error_Pages/Error_PagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Error_PagesApp.swift
+//  Error_Pages
+//
+//  Created by Abu Anwar MD Abdullah on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Error_PagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
